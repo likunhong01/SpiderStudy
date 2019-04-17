a9a6a48f6390e94b3d69aa06822c3647c6c0f529
likunhong01 <42308753+likunhong01@users.noreply.github.com>
2019-04-18 00:15:53 +0800

    糗事百科爬虫

---
+++ no04通用爬虫/QiushibaikeSpider.py
@@ -0,0 +1,114 @@
+#coding=utf-8
+#Version:python3.6.0
+#Tools:Pycharm 2017.3.2
+# Author:LIKUNHONG
+__date__ = '2019/4/17 19:10'
+__author__ = 'likunkun'
+
+import requests
+import re
+import json
+from lxml import etree
+
+class QiuShiBaiKe:
+    def __init__(self):
+        # 起始地址
+        self.start_url = 'https://www.qiushibaike.com/text/page/1/'
+        # 根网址
+        self.root_url = 'https://www.qiushibaike.com'
+        # 请求头
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6784.400 QQBrowser/10.3.2667.400"
+        }
+
+    # 发送请求，获取响应
+    def parse_url(self, url):
+        response = requests.get(url, headers=self.headers)
+        return response.content
+
+    def get_content(self, html_str):
+        # 转化为HTML对象
+        html = etree.HTML(html_str)
+        # 找到所有的段子的div的父亲标签（包含了所有段子div的div）
+        divs = html.xpath('//*[@id="content-left"]/div')
+        # 用一个列表保存段子，每个段子有作者和内容（字典）
+        duanzis = []
+        # 对每个段子循环
+        for div in divs:
+            duanzi = {}
+            # 获取作者
+            duanzi['author'] = div.xpath('.//h2/text()')[0].replace('\n', '') if len(div.xpath('.//h2/text()')) > 0 else None
+
+            # 获取内容，因为内容放在了不同标签中，需要连起来
+            if len(div.xpath('.//div[@class="content"]/span/text()')) > 0:
+                strs = div.xpath('.//div[@class="content"]/span/text()')
+                content = ''
+                for str in strs:
+                    content += str
+                duanzi['content'] = content.replace('\n', '')
+            else:
+                duanzi['content'] = None
+
+            # 打印测试
+            # print(duanzi['author'] + '说：' + duanzi['content'])
+            # 把这个段子放进列表
+            duanzis.append(duanzi)
+
+        # 获取下一页url
+        next_url = self.root_url + html.xpath('//*[@id="content-left"]/ul/li[8]/a/@href')[0] if len(
+            html.xpath('//*[@id="content-left"]/ul/li[8]/a/@href')) > 0 else None
+        return next_url, duanzis
+
+    # 保存段子
+    def save_duanzi(self,duanzis):
+        file_path = '段子.txt'
+        with open(file_path, 'a', encoding='utf-8') as f:
+            for duanzi in duanzis:
+                str = duanzi['author'] + '说：' + duanzi['content']
+                f.write(json.dumps(str, ensure_ascii=False, indent=2))  # indent是每次递进的空格
+                f.write('\n')
+
+    # 处理逻辑
+    def run(self):
+        # 下一个要获取的网页地址，第一次是起始地址
+        next_url = self.start_url
+        while next_url is not None:
+            # 发送请求，获取响应
+            html_str = self.parse_url(next_url)
+
+            # 提取数据，提取下一页url
+            next_url, duanzis = self.get_content(html_str)
+
+            # 保存数据
+            self.save_duanzi(duanzis)
+
+if __name__ == '__main__':
+    qiu = QiuShiBaiKe()
+    qiu.run()
+
+
+# url = 'https://www.qiushibaike.com/text/page/1/'
+# headers = {
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6784.400 QQBrowser/10.3.2667.400"
+# }
+# response = requests.get(url,headers=headers)
+# html_str = response.content.decode()
+#
+# # print(html_str)
+# html = etree.HTML(html_str)
+#
+# divs = html.xpath('//*[@id="content-left"]/div')
+#
+# for div in divs:
+#     duanzi = {}
+#     duanzi['author'] =  div.xpath('.//h2/text()')[0].replace('\n','')
+#     strs = div.xpath('.//div[@class="content"]/span/text()')
+#     content = ''
+#     for str in strs:
+#         content += str
+#     duanzi['content'] = content.replace('\n','')
+#     print(duanzi['author']+'说：'+duanzi['content'])
+#
+# root_url = 'https://www.qiushibaike.com'
+# next_url = root_url + html.xpath('//*[@id="content-left"]/ul/li[8]/a/@href')[0]
+# print(next_url)
--- no04通用爬虫/TiebaSpider.py
+++ no04通用爬虫/TiebaSpider.py
@@ -2,55 +2,76 @@ import requests
 import json
 import re
 from lxml import etree
- 
+
+
 class TiebaSpider:
-	def __init__(self,tieba_name):
-		self.tieba_name = tieba_name
-		self.start_url = '' + tieba_name + ''
-		self.headers = ''
+    def __init__(self, tieba_name):
+        self.tieba_name = tieba_name
+        self.start_url = "http://tieba.baidu.com/mo/q----,sz@320_240-1-3---2/m?kw=" + tieba_name + "&pn=0"
+        self.part_url = "http://tieba.baidu.com/mo/q----,sz@320_240-1-3---2/"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Mobile Safari/537.36"
+        }
 
-	def parse_url(self, url):
-		response = requests.get(url,headers=self.headers)
-		return response.content.decode()
-		
-	def get_content_list(self, html_str):	# 提取数据
-		html = etree.HTML(html_str)
-		div_list = html.xpath('//div[contains(@class,"i")]')	# 根据div分组
-		content_list = []
-		for div in div_list:
-			item = {}
-			item['title'] = div.xpath('./a/text()')[0] if len(div.xpath('./a/text()')) > 0 else None
-			item['href'] = div.xpath('./a/@href')[0] if len(div.xpath('./a/@href')) > 0 else None
-			item['img_list'] = self.get_img_list(item['href'])
-			content_list.append(item)
-		# 提取下一页url地址
-		next_url = html.xpath('//a[text()="下一页"')
-		return content_list,next_url
+    def parse_url(self, url):
+        print(url)
+        response = requests.get(url, headers=self.headers)
+        return response.content
 
-	def get_img_list(self, detail_url):	# 获取帖子中的所有图片
-		# 3.2-3.4
-		return img_list
-		pass
+    def get_content_list(self, html_str):  # 提取数据
+        html = etree.HTML(html_str)
+        div_list = html.xpath('//div[contains(@class,"i")]')  # 根据div分组
+        content_list = []
+        for div in div_list:
+            item = {}
+            item['title'] = div.xpath('./a/text()')[0] if len(div.xpath('./a/text()')) > 0 else None
+            item['href'] = self.part_url + div.xpath('./a/@href')[0] if len(div.xpath('./a/@href')) > 0 else None
+            item['img_list'] = self.get_img_list(item['href'], [])
+            item['img_list'] = [requests.utils.unquote(i).split('src=')[-1] for i in item['img_list']]
+            content_list.append(item)
+        # 提取下一页url地址
+        next_url = self.part_url + html.xpath('//a[text()="下一页"]/@href')[0] if len(
+            html.xpath("//a[text()='下一页']/@href")) > 0 else None
+        return content_list, next_url
 
-	def sava_content_list(self,content_list):	# 保存数据
-		file_path = self.tieba_name + '.txt'
-		with open(file_path, 'a',encoding='utf-8') as f:
-			for content in content_list:
-				f.write(json.dumps(content, ensure_ascii=False, indent=2))
-				f.write('\n')
+    def get_img_list(self, detail_url, total_img_list):  # 获取帖子中的所有图片
+        # 3.2提取列表页的url地址，获取详情页的第一页
+        detail_html_str = self.parse_url(detail_url)
+        detail_html = etree.HTML(detail_html_str)
+        # 3.3提取详情页第一页的图片，提取下一页的地址
+        img_list = detail_html.xpath('//img[@class="BDE_Image"]/@src')
+        total_img_list.extend(img_list)
+        # 3.4请求详情页下一页的地址，进入循环3.2-3.4
+        detail_next_url = detail_html.xpath('//a[text()="下一页"]/@href')
+        if len(detail_next_url) > 0:
+            detail_next_url = self.part_url + detail_next_url[0]
+            return self.get_img_list(detail_next_url, total_img_list)
+        return total_img_list
 
-	def run(self):
-		next_url = self.start_url
-		while next_url is not None:
-			# 1.start_url
-			# 2.发送请求，获取响应
-			html_str = self.parse_url(next_url)
-			# 3.提取数据，提取下一页的url地址
-			# 3.1提取列表页的url地址和标题
-			# 3.2提取列表页的url地址，获取详情页的第一页
-			# 3.3提取详情页第一页的图片，提取下一页的地址
-			# 3.4请求详情页下一页的地址，进入循环3.2-3.4
-			content_list,next_url = self.get_content_list(html_str)
-			# 4.保存数据
-			self.sava_content_list(content_list)
-			# 5.请求下一页的url地址，进入循环2-5
+    def sava_content_list(self, content_list):  # 保存数据
+        file_path = self.tieba_name + '.txt'
+        with open(file_path, 'a', encoding='utf-8') as f:
+            for content in content_list:
+                f.write(json.dumps(content, ensure_ascii=False, indent=2))
+                f.write('\n')
+
+    def run(self):
+        next_url = self.start_url
+        while next_url is not None:
+            # 1.start_url
+            # 2.发送请求，获取响应
+            html_str = self.parse_url(next_url)
+            # 3.提取数据，提取下一页的url地址
+            # 3.1提取列表页的url地址和标题
+            # 3.2提取列表页的url地址，获取详情页的第一页
+            # 3.3提取详情页第一页的图片，提取下一页的地址
+            # 3.4请求详情页下一页的地址，进入循环3.2-3.4
+            content_list, next_url = self.get_content_list(html_str)
+            # 4.保存数据
+            self.sava_content_list(content_list)
+    # 5.请求下一页的url地址，进入循环2-5
+
+
+if __name__ == '__main__':
+    tieba = TiebaSpider('做头发')
+    tieba.run()
