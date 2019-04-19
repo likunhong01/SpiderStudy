4b34e2b49457470536e37463e7a92d4d4523fe2a
likunhong01 <42308753+likunhong01@users.noreply.github.com>
2019-04-19 19:32:48 +0800

    selenium模拟登陆

---
+++ no05动态html/02豆瓣登录.py
@@ -0,0 +1,42 @@
+#coding=utf-8
+#Version:python3.6.0
+#Tools:Pycharm 2017.3.2
+# Author:LIKUNHONG
+__date__ = '2019/4/19 15:23'
+__author__ = 'likunkun'
+
+from selenium import webdriver
+import requests
+
+# 实例化
+driver = webdriver.PhantomJS()
+driver.get('https://www.douban.com/')
+
+# 填充用户名密码
+driver.find_element_by_id('username').send_keys('18679959330')
+driver.find_element_by_id('password').send_keys('123')
+
+'''这里可能有验证码，如果有验证码就用云打码平台搞定'''
+# 获取图片url地址
+yzm_url = driver.find_element_by_id('captcha_image').get_attribute('src')
+yzm_content =requests.get(yzm_url).content
+
+# 云打码获取
+from yundama.dama import indetify
+yzm_code = indetify(yzm_content)
+
+# 输入验证码
+driver.find_element_by_id('captcha_field').send_keys(yzm_code)
+
+
+# 点击提交
+driver.find_element_by_class_name('btn-account').click()
+
+# 获取cookies
+cookies = {i['name']:i['value'] for i in driver.get_cookies()}
+print(cookies)
+
+import time
+time.sleep(5)
+driver.quit()
+
---
+++ no05动态html/03selenium更多操作.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+#Version:python3.6.0
+#Tools:Pycharm 2017.3.2
+# Author:LIKUNHONG
+__date__ = '2019/4/19 19:32'
+__author__ = 'likunkun'
